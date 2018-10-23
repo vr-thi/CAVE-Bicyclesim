6cf77d1c1e7fcf9ca35350436e00d02429964665
Armin <armin.becher@thi.de>
2018-10-23 08:20:04 +0200

    Tamara: Updated AR-HUD

---
+++ Assets/triggerDirection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerDirection : MonoBehaviour {
+
+    public string direction;
+    public ArHudController ARHUD;
+    private Sprite directionImage;
+
+	// Use this for initialization
+	void Start () {
+        directionImage = Resources.Load<Sprite>("Images/" + direction);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ARHUD.SetDirectionIndicator(true, directionImage);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ARHUD.SetDirectioniIndicator();
+    }
+
+
+}
